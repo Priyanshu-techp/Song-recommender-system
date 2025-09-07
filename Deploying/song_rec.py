c34ef18fb11809ce8ab346e1e70221e1cb005204
Priyanshu pandey <pp208077@gmail.com>
2025-09-07 21:07:30 +0530

    song_rec.py

--- Deploying/song_rec.py
+++ Deploying/song_rec.py
@@ -32,24 +32,23 @@ if st.button('Recommend Song'):
     recommandations = recommand(selected_song)
     if recommandations:
         st.subheader("Recommended Songs")
-        cols = st.columns(2)
+        cols = st.columns(3)
 
         for i, url in enumerate(recommandations):
             embed_url = url.replace("open.spotify.com/track/", "open.spotify.com/embed/track/")
-            with cols[i % 2]:
-                    st.markdown(
-                        """
-                          <div style="border-radius:15px; 
-                          background-color:#1DB95420; 
-                          padding:20px; 
-                          margin-bottom:25px; 
-                          box-shadow: 0 6px 14px rgba(0,0,0,0.3);">
-                        ""","""
-                        unsafe_allow_html=True
-            )
-
-            components.iframe(embed_url, width=400, height=200, scrolling=False)
-            st.markdown("</div>", unsafe_allow_html=True)
+            with cols[i % 3]:
+                st.markdown(
+                    """
+                    <div style="border-radius:15px; 
+                                background-color:#1DB95420; 
+                                padding:20px; 
+                                margin-bottom:25px; 
+                                box-shadow: 0 6px 14px rgba(0,0,0,0.3);">
+                    """,
+                    unsafe_allow_html=True
+                )
+                components.iframe(embed_url, width=300, height=400, scrolling=False)
+                st.markdown("</div>", unsafe_allow_html=True)
 
 
 
